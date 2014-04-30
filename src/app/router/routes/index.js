a628d6e4505882ec1e6bb64054cb18f9e670ae17
Alexey Volkov <alexey.wild88@gmail.com>
2014-04-30 14:07:49 +0300

    fix

--- src/app/router/routes/index.js
+++ src/app/router/routes/index.js
@@ -1,7 +1,16 @@
 module.exports.index = function (req, res) {
     /** @todo Проверять доступность создания виджета (widget_uid + origin) */
     // Устанавливаем UID виджета в cookie
-    if (typeof res.cookie.widget_uid === 'undefined' && req.params.widget_uid) {
+    if (req.params && req.params.widget_uid) {
+        // Очищаем данные от других виджетов
+        if (typeof res.cookie.widget_uid != 'undefined'){
+            if (res.cookie.widget_uid != req.params.widget_uid) {
+                res.cookie.chat = JSON.stringify({});
+                res.cookie.url = JSON.stringify('');
+                res.cookie.opened = JSON.stringify(false);
+                res.cookie.auth = JSON.stringify(false);
+            }
+        }
         res.cookie('widget_uid', JSON.stringify(req.params.widget_uid));
     }
 
